b56c82cc473076a8fd92cf56beceefe4d92b05ee
nsankar2 <nsankar2@IN-PG02M18F>
2021-10-25 11:39:07 +0530

    Assignment

---
+++ FirstWeekAssignment/src/Hackerrank/Javasubstring.java
@@ -0,0 +1,13 @@
+package Hackerrank;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Javasubstring {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = "helloworld";
+        System.out.println(S.substring(3,7));
+    }
+}
