51dd7d02aa0382438e709496020f5300b46bc73a
nymishasankar62 <92718747+nymishasankar62@users.noreply.github.com>
2021-10-25 11:28:58 +0530

    Initial commit

---
+++ DXC/src/objectclass/util.java
@@ -0,0 +1,13 @@
+package objectclass;
+/**
+ * this class has all utility methods
+ * @author Admin
+ *
+ */
+public class util {
+	public static String COMPANY_NAME = "dxc";
+	public static int incrementByTen(int no) {
+		return no+10;
+	}
+
+}
